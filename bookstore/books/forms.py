fb0e6eedf09ecf52a8f936bc6318ad707af01abc
Konnarm <xellosus@vp.pl>
2016-09-19 19:53:29 +0200

    Powiedzmy ze cos jest

--- bookstore/books/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import Writer, Book
-
-class BookForm(forms.ModelForm):
-
-    class Meta:
-        model = Book
-        fields = ['book_title', 'writer', 'book_cover', 'genre', 'series']
